6a704cc7a1d1746d5c9a6a774aecd037295fe38f
Vikrant Thakur <118067552+vikranttdev@users.noreply.github.com>
2023-01-10 17:16:17 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,4 +132,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vikrantt409@gmail.com'
-EMAIL_HOST_PASSWORD = 'ofzoqibvkuqnjgtj'
+EMAIL_HOST_PASSWORD = '' #put your password here
